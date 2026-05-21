cfba9d62875e718aaa44e3d11b9af66f0c00fca0
Manish673-jpg <manishpanday037@gmail.com>
2026-05-21 10:05:23 +0200

    updated

--- src/learning_model.py
+++ src/learning_model.py
@@ -1,44 +1,12 @@
 import scipy.io
 import scipy.signal
-from scipy.signal import butter,filtfilt
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats
+import sklearn
+from Signalverarbeitung import signal_length_matching,signals,bearing_data
 
-
-#importing mydata
-OR = scipy.io.loadmat(r'C:\Users\manis\Documents\Masters\self project\Data\12k_Drive_End_OR007@6_0')
-IR = scipy.io.loadmat(r'C:\Users\manis\Documents\Masters\self project\Data\12k_Drive_End_IR007@6_0')
-Ball = scipy.io.loadmat(r'C:\Users\manis\Documents\Masters\self project\Data\12k_Drive_End_BA007@6_0')
-NOR = scipy.io.loadmat(r'C:\Users\manis\Documents\Masters\self project\Data\normaldata')
-
-
-#Signals & Frequencies
-signals = [NOR['X097_DE_time'].ravel(),
-           OR['X130_DE_time'].ravel(),
-           IR['X278_DE_time'].ravel(),
-           Ball['X282_DE_time'].ravel(),
-           ]
-
-
-bearing_data = {
-    'pitch_diameter' : 38.5,
-    'ball_diameter' : 7.94,
-    'n' : 9,
-    'phi' : 0,
-    'shaft_speed' : 1797
-
-}
-
-
-def signal_length_matching(sgn):
-    min_length = min(len(signal) for signal in sgn)
-
-    for i in range(len(sgn)):
-        sgn[i] = sgn[i][:min_length]
-
-    return sgn
 
 def extract_features(sgn,window_size = 1024):
     features = []
@@ -71,13 +39,9 @@ def build_dataset(sgn):
 
     return df
 
+
+
 Signals = signal_length_matching(signals)
 windowed_signal = extract_features(Signals[0],window_size=1024)
 
 df = build_dataset(Signals)
-df.to_csv(r"C:\Users\manis\Documents\Masters\self project\Data\data_features\bearing_feature.csv", index=False)
-
-
-
-df.boxplot(column='Kurtosis', by='Label')
-plt.show()
