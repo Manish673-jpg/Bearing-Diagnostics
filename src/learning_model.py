fff9f0600d7ef8d4a78632e54306939d3fa79b57
Manish673-jpg <manishpanday037@gmail.com>
2026-05-23 22:12:34 +0200

    random_classifier

--- src/learning_model.py
+++ src/learning_model.py
@@ -1,47 +1,42 @@
-import scipy.io
-import scipy.signal
+from sklearn.preprocessing import StandardScaler
+from Signalverarbeitung import Signals,bearing_data
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats
 import sklearn
-from Signalverarbeitung import signal_length_matching,signals,bearing_data
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report,confusion_matrix,ConfusionMatrixDisplay
 
+##my data frame
 
-def extract_features(sgn,window_size = 1024):
-    features = []
-    n_windows = len(sgn) // window_size
-    trimmed = np.array(sgn[:n_windows * window_size])
-    windows = trimmed.reshape(n_windows, window_size)
+df = pd.read_csv(r"C:\Users\manis\Documents\Masters\self project\Data\data_features\bearing_feature.csv")
 
-    for window in windows:
-        rms = np.sqrt(np.mean(window**2))
-        kurt = scipy.stats.kurtosis(window)
-        crest = np.max(window)/rms
-        skew = scipy.stats.skew(window)
-        peak_to_peak = np.max(window)-np.min(window)
-        features.append([rms, kurt,crest, skew, peak_to_peak])
+y = df['Label']
+x= df.drop('Label',axis=1)
 
-    return np.array(features)
+X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=0.2,random_state=42,stratify=y)
 
-def build_dataset(sgn):
-    values = []
-    labels = []
-    for i in range(len(sgn)):
-        my_value = extract_features(sgn[i],window_size=1024)
-        values.append(my_value)
-        labels.append(np.full(len(values[i]),i))
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
-    all_features = np.vstack(values)
-    all_labels = np.concatenate(labels)
-    df = pd.DataFrame(all_features,columns=['RMS', 'Kurtosis','Crest_factor','Skewness','Peak_to_Peak'])
-    df['Label'] = all_labels
+print(X_train.shape)
+print(Y_train.value_counts())
 
-    return df
+model  = RandomForestClassifier(n_estimators=100,random_state=42)
+model.fit(X_train,Y_train)
+y_pred = model.predict(X_test)
 
+print(accuracy_score(Y_test,y_pred))
+print(classification_report(Y_test,y_pred))
+ConfusionMatrixDisplay.from_predictions(Y_test,y_pred)
 
-
-Signals = signal_length_matching(signals)
-windowed_signal = extract_features(Signals[0],window_size=1024)
-
-df = build_dataset(Signals)
+importances = model.feature_importances_
+feature_names = ['RMS', 'Kurtosis', 'Crest_factor', 'Skewness', 'Peak_to_Peak']
+plt.figure()
+plt.bar(feature_names, importances)
+plt.title('Feature Importance')
+plt.show()
